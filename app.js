fcf458c486647a6cad00a36904d49e30d718ac2d
sreeragp07 <84373517+sreeragp07@users.noreply.github.com>
2022-08-29 18:53:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //var num=Math.floor(Math.random()*4)+1;
 
 $('#card').wScratchPad({
-    size        : 10,          // The size of the brush/scratch.
+    size        : 20,          // The size of the brush/scratch.
     bg          : 'images/sam3.jpg',  // Background (image path or hex color).
     fg          :  'images/sam1.jpg',  // Foreground (image path or hex color).
    
